319c8d960629c01dd663c943b622b85be9ed52ee
omerserrdar <omerserrdar@gmail.com>
2025-10-25 10:30:24 +0300

    Proje dosyası ekle.

---
+++ Speech to Text/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace SpeechToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(-2, 424);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(257, 81);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Location = new System.Drawing.Point(303, 424);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(243, 81);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Location = new System.Drawing.Point(12, 12);
+            this.txtSonuc.Multiline = true;
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(523, 372);
+            this.txtSonuc.TabIndex = 2;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(254, 508);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(38, 16);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "Hazır";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 558);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label lblDurum;
+    }
+}
+
---
+++ Speech to Text/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+namespace SpeechToText
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine recognizer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeRecognizer();
+        }
+
+        private void InitializeRecognizer()
+        {
+            try
+            {             
+                var cultureInfo = new System.Globalization.CultureInfo("en-US");
+                var recognizerInfo = SpeechRecognitionEngine.InstalledRecognizers()
+                    .FirstOrDefault(r => r.Culture.Equals(cultureInfo));
+                if (recognizerInfo == null)
+                {
+                    MessageBox.Show("Türkçe (tr-TR) konuşma paketi bulunamadı.\nLütfen Windows Ayarlar > Zaman ve Dil > Konuşma bölümünden Türkçe dil paketini yükleyin.", "Hata");
+                    btnBaslat.Enabled = false;
+                    btnDurdur.Enabled = false;
+                    lblDurum.Text = "Türkçe paketi yüklü değil.";
+                    return;
+                }               
+                recognizer = new SpeechRecognitionEngine(recognizerInfo);
+
+                recognizer.SetInputToDefaultAudioDevice(); 
+                recognizer.LoadGrammar(new DictationGrammar());
+                recognizer.SpeechRecognized += Recognizer_SpeechRecognized; 
+                recognizer.SpeechRecognitionRejected += Recognizer_SpeechRecognitionRejected; 
+                btnDurdur.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Tanıyıcı başlatılırken hata oluştu: {ex.Message}", "Kritik Hata");
+            }
+        }
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (recognizer == null) return;
+
+            try
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                btnBaslat.Enabled = false;
+                btnDurdur.Enabled = true;
+                lblDurum.Text = "Dinliyorum...";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Dinleme başlatılamadı: {ex.Message}");
+            }
+        }
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            if (recognizer == null) return;
+            recognizer.RecognizeAsyncStop();
+            btnBaslat.Enabled = true;
+            btnDurdur.Enabled = false;
+            lblDurum.Text = "Durduruldu.";
+        }
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtSonuc.AppendText(e.Result.Text + Environment.NewLine);
+            });
+        }
+        private void Recognizer_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                lblDurum.Text = "Anlaşılamadı. Tekrar deneyin.";
+            });
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (recognizer != null)
+            {
+                recognizer.Dispose();
+            }
+        }
+    }
+}
